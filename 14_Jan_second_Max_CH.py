cfd0f65a0853cc95963c8ec487c89eee654ef29e
Sakshi Kumari <Ksakshi489@gmail.com>
2021-02-25 23:09:53 +0530

    Second Largest Element

---
+++ 14_Jan_second_Max_CH.py
@@ -0,0 +1,9 @@
+#SECOND MAXIMUM ELEMENT IN A DATA
+
+
+data = [56, 89, 45, 54, 77, 66, 1, 101]
+#new_set = set(data)
+#new_list = list(new_set)
+data.sort()
+print("The second maximum element is", data[-2])
+
